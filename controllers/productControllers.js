eb716e8340942141977d10db50db16a34b7632f8
kriteemmuu <Kritimakhatri123@gmail.com>
2024-05-31 13:00:01 +0545

    changes

---
+++ controllers/productControllers.js
@@ -0,0 +1,88 @@
+const path = require("path");
+const ProductModel = require("../models/productModel");
+
+const createProduct = async (req, res) => {
+    // Check incoming data
+    console.log(req.body);
+    console.log(req.files);
+
+    // Destructuring the body data
+    const { productName, productPrice, productCategory, productDescription } = req.body;
+
+    // Validation
+    if (!productName || !productPrice || !productCategory || !productDescription) {
+        return res.status(400).json({
+            success: false,
+            message: "Please enter all fields!",
+        });
+    }
+
+    // Validate if there is an image
+    if (!req.files || !req.files.productImage) {
+        return res.status(400).json({
+            success: false,
+            message: "Image not found!",
+        });
+    }
+
+    const { productImage } = req.files;
+    
+    // Upload image
+    // 1. Generate new image name (abc.png) -> need to change to sthg like (213-qbc.png)
+    const imageName = `${Date.now()}-${productImage.name}`;
+    
+    // 2. Make an upload path (/path/upload-directory)
+    const imageUploadPath = path.join(__dirname, `../public/products/${imageName}`);
+    
+    // 3. Move to that directory (await, try-catch)
+    try {
+        await productImage.mv(imageUploadPath);
+
+        // Save to database
+        const newProduct = new ProductModel({
+            productName: productName,
+            productPrice: productPrice,
+            productCategory: productCategory,
+            productDescription: productDescription,
+            productImage: imageName,
+        });
+
+        const product = await newProduct.save();
+        res.status(201).json({
+            success: true,
+            message: "Product created successfully!",
+            data: product,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: "Internal server error!",
+            error: error,
+        });
+    }
+};
+
+// Fetch all products
+const getAllProducts = async (req, res) => {
+    try {
+        const allProducts = await ProductModel.find({});
+        res.status(201).json({
+            success: true,
+            message: "Products successfully fetched",
+            products: allProducts,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: "Internal server error",
+            error: error,
+        });
+    }
+};
+
+module.exports = {
+    createProduct,
+    getAllProducts,
+};
---
+++ routes/productRoutes.js
@@ -0,0 +1,13 @@
+const router= require('express').Router();
+const productController = require('../controllers/productControllers')
+
+
+router.post('/create', productController.createProduct)
+
+//fetch all products
+router.get('/get_all_products',productController.getAllProducts)
+
+
+
+// exporting the router
+module.exports = router
